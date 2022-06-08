b3cb6b49040115f18c16c2e8a1c1f0b6a03b9edd
tanxch <qq1303343970@gmai.com>
2022-06-08 16:26:36 +0800

    master

--- main.cpp
+++ main.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
-struct main
+struct WEB
 {
-    /* data */
+    int number;
+    string website;
+    string title;
+    string text;
 };
 
 int main()
 {
+    WEB *web = new WEB;
+    ifstream openfile;
+    openfile.open("news.csv",ios::in);
+    ofstream writefile;
+    writefile.open("pages.txt",ios::out|ios::trunc);
+    string buffer;
+    web->number = 1;
+    while(getline(openfile,buffer))
+    {
+        int pos = buffer.find("http://");
+        if(pos != -1)
+        {
+            int end = buffer.find(".html");
+            web->title = buffer.substr(0,end+5);
+            // cout<<web->number<<"."<<web->title<<endl;
+            writefile<<web->number<<"."<<web->title<<endl;
+            web->number++;
+        }
+    }
+    writefile.close();
     return 0;
 }
